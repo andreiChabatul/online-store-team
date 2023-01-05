20193336c1da05cef730d855f584bf7dc96f08dd
andreiChabatul <andreichabatul@gmail.com>
2023-01-05 19:18:59 +0300

    fix: add new elements at const file

--- develop/src/CONST/const.ts
+++ develop/src/CONST/const.ts
@@ -1,4 +1,8 @@
-import { IPromoCode } from '../types/index';
+import { IPopUpItem, IPromoCode } from '../types/index';
+import mirCard from '../assets/img/mirCard.png';
+import sberCard from '../assets/img/sberCard.png';
+import tinCard from '../assets/img/tinCard.png';
+
 export const BODY = document.querySelector('body');
 export const URL_API = 'https://dummyjson.com/products?limit=25';
 export const PROMO_CODE: IPromoCode[] = [
@@ -18,3 +22,51 @@ export const PROMO_CODE: IPromoCode[] = [
         text: 'Chabatul Andrei - 0%',
     },
 ];
+export const POP_UP: IPopUpItem[] = [
+    {
+        name: 'name',
+        regEx: '^[a-zа-я]{3,}(\\s[a-zа-я]{3,})+',
+        placeHolder: 'Name',
+        maxLenght: 'Infinity',
+    },
+    {
+        name: 'phone',
+        regEx: '^\\+\\d{9,}$',
+        placeHolder: 'Phone number',
+        maxLenght: 'Infinity',
+    },
+    {
+        name: 'address',
+        regEx: '^[A-Za-zА-Яа-я0-9]{5,}(\\s[A-Za-zА-Яа-я0-9]{5,}){2,}',
+        placeHolder: 'Delivery address',
+        maxLenght: 'Infinity',
+    },
+    {
+        name: 'email',
+        regEx: '^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(.\\w{2,3})+$',
+        placeHolder: 'E-mail',
+        maxLenght: 'Infinity',
+    },
+];
+export const POP_UP_CARD: IPopUpItem[] = [
+    {
+        name: 'numberCard',
+        regEx: '^\\d{4}(\\s\\d{4}){3}$',
+        placeHolder: 'Card number',
+        maxLenght: '19',
+    },
+    {
+        name: 'dataCard',
+        regEx: '(0[1-9]|1[0-2])/([0-9]{2})',
+        placeHolder: 'Valid Th',
+        maxLenght: '5',
+    },
+    {
+        name: 'CVV',
+        regEx: '^\\d{3}$',
+        placeHolder: 'Code',
+        maxLenght: '3',
+    },
+];
+export const CARD_IMG: string[] = [mirCard, tinCard, sberCard];
+export const eventsInput: string[] = ['blur', 'input', 'change'];
