4b346abe423551ae22d68ab4bc70628824bbefba
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-01-06 21:57:21 +0300

    feat: add update method in moreInfo

--- develop/src/pages/moreInfo/slider.ts
+++ develop/src/pages/moreInfo/slider.ts
@@ -1,7 +1,6 @@
 import CreateElement from '../../utils/CreateElement';
 import { IProduct, IRenderComponent } from '../../types/index';
-import './ slider.css';
-
+import './slider.css';
 
 export default class Slider implements IRenderComponent {
   obj: IProduct;
@@ -10,28 +9,32 @@ export default class Slider implements IRenderComponent {
   }
 
   render(): HTMLDivElement {
-    let a: string = '0';
+    let a = '0';
     const prodactPhotos = CreateElement.createDivElement('moreinfo-prodact-photos');
     const grandPhoto = CreateElement.createDivElement('moreinfo-grand-foto');
     const slides = CreateElement.createDivElement('moreinfo-slides');
-    const productImgGrand = CreateElement.createImgElement('moreinfo-product-img-grand', `${this.obj.images[0]}`, '');
+    const productImgGrand = CreateElement.createImgElement('moreinfo-product-img-grand', `${this.obj.images[0]}`);
     prodactPhotos.append(grandPhoto);
     prodactPhotos.append(slides);
     this.obj.images.forEach((element: string, index: number) => {
-      let productImgSLide1 = CreateElement.createImgElement('moreinfo-product-img-slide', `${element}`, `${index}`);
+      const productImgSLide1 = CreateElement.createImgElement(
+        'moreinfo-product-img-slide',
+        `${element}`,
+        `${index}`
+      );
       if (productImgSLide1.id === a) {
         productImgSLide1.classList.add('moreinfo-active-slide');
       }
-      productImgSLide1.addEventListener("click", function showConsole(e) {
+      productImgSLide1.addEventListener('click', function showConsole() {
         if (productImgSLide1.id !== a) {
-          let activeSlide = document.getElementById(`${a}`) as HTMLImageElement;
+          const activeSlide = document.getElementById(`${a}`) as HTMLImageElement;
           a = productImgSLide1.id;
-          productImgGrand.src = productImgSLide1.src
-          console.log(activeSlide)
+          productImgGrand.src = productImgSLide1.src;
+          console.log(activeSlide);
           productImgSLide1.classList.add('moreinfo-active-slide');
           activeSlide.classList.remove('moreinfo-active-slide');
         }
-      })
+      });
       slides.append(productImgSLide1);
     });
     grandPhoto.append(productImgGrand);
