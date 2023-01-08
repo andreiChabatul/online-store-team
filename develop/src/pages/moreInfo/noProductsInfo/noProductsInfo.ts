6b4b91b67205a7b2bbc2b417363ff2c8dcd2b4ba
andreiChabatul <andreichabatul@gmail.com>
2023-01-08 10:34:46 +0300

    refractor: clean code more info page at eslint

--- develop/src/pages/moreInfo/noProductsInfo/noProductsInfo.ts
+++ develop/src/pages/moreInfo/noProductsInfo/noProductsInfo.ts
@@ -3,18 +3,18 @@ import CreateElement from '../../../utils/CreateElement';
 import './noProductsInfo.css';
 
 export default class NoProductsInfo implements IRenderComponent {
-  id: number;
-  constructor(id: number) {
-    this.id = id;
-  }
+    id: number;
+    constructor(id: number) {
+        this.id = id;
+    }
 
-  render(): HTMLDivElement {
-    const containerNoFound = CreateElement.createDivElement('container-noFounds');
-    const textNoFound = CreateElement.createParagraphElement(
-      'container-noFounds__text',
-      `Product number ${this.id} not found`
-    );
-    containerNoFound.append(textNoFound);
-    return containerNoFound;
-  }
+    render(): HTMLDivElement {
+        const containerNoFound = CreateElement.createDivElement('container-noFounds');
+        const textNoFound = CreateElement.createParagraphElement(
+            'container-noFounds__text',
+            `Product number ${this.id} not found`
+        );
+        containerNoFound.append(textNoFound);
+        return containerNoFound;
+    }
 }
